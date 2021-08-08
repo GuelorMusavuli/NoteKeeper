fe61f9909da216f7791b4238e29c64206ffcbbb8
Guelor Musavuli <guelormusavuli2@gmail.com>
2021-08-08 02:18:32 -0700

    Implementing DataManager, accessing data from activities  and activities relationship

---
+++ app/src/main/java/com/guelmus/android/notekeeper/MainActivity.kt
@@ -0,0 +1,105 @@
+
+package com.guelmus.android.notekeeper
+
+import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    //Determine whether a note position was set or not
+    private var notePosition = POSITION_NOT_SET
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        //Populate spinner with courses from DataManager
+        val courseAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,//format the current selection
+            DataManager.courses.values.toList()//collection of courses retrieve from the map
+        ) //format the selections that are displayed within the drop-down list
+        courseAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerCourses.adapter = courseAdapter
+
+        //Retrieved back the note position from the extra within intent
+        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+
+        //if there is a notePosition, populate the views
+        if (notePosition != POSITION_NOT_SET)
+            displayNote()
+
+    }
+    /** Populate views in the main activity with notes from  intent extra*/
+    private fun displayNote() {
+        //Get note corresponding to the notePosition and set the note's title and text
+        // to the string values contained in the note
+        val note = DataManager.notes[notePosition]
+        textNoteTitle.setText(note.noteTitle)
+        textNoteText.setText(note.noteContent)
+
+        //Select the appropriate course from the spinner
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        spinnerCourses.setSelection(coursePosition)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    /**Handle menu click*/
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            R.id.action_next -> {
+                moveNext()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    /**Move t the next note on the screen */
+    private fun moveNext() {
+        ++notePosition
+        displayNote()
+
+        //Trigger change to the menu item at runtime
+        invalidateOptionsMenu()
+    }
+
+    /**Change menu state*/
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+
+        //If user reaches the last notes, changes the appearance of the menu icon
+        if(notePosition >= DataManager.notes.lastIndex) {
+            Toast.makeText(this, "You're at the last note!", Toast.LENGTH_SHORT).show()
+            //Reference the menu to be changed
+            val menuItem = menu?.findItem(R.id.action_next)
+            if(menuItem != null) {
+                //Change the next menu icon if at last index and disable click on it
+                menuItem.icon = getDrawable(R.drawable.ic_block_white_24dp)
+                menuItem.isEnabled = false
+
+            }
+        }
+
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+
+}
---
+++ app/src/main/java/com/guelmus/android/notekeeper/NoteKeeperData.kt
@@ -0,0 +1,19 @@
+package com.guelmus.android.notekeeper
+
+/**
+ * Class that represent a course
+ * */
+data class CourseInfo (val courseId : String, val courseTitle : String) {
+    override fun toString(): String {
+        //Value to be displayed on the spinner
+        return courseTitle
+    }
+}
+
+/**
+ * Class that represent a note associated with a course
+ * */
+data class NoteInfo(var course: CourseInfo? = null,
+                    var noteTitle: String? = null, var noteContent: String? = null)
+
+
